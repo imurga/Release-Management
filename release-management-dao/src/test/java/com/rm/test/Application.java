97aec82ebfa59c9e63c94db14014a4a6ab6bc552
imurga <ingrid.murga@gmail.com>
2016-06-06 23:35:54 -0500

    removing spring-config

--- release-management-dao/src/test/java/com/rm/test/Application.java
+++ release-management-dao/src/test/java/com/rm/test/Application.java
@@ -6,27 +6,29 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 import com.rm.dao.entity.User;
-import com.rm.dao.repo.UserRepo;
+import com.rm.dao.repo.UserRepository;
 
 public class Application {
-	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				new ClassPathResource("spring-config.xml").getPath());
-		UserRepo userRepo = context.getBean(UserRepo.class);
+	
+	
+	public static void main(String[] args){
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                new ClassPathResource("spring-config.xml").getPath());
+        UserRepository userRepo = context.getBean(UserRepository.class);
 
-		User user = new User();
-		user.setUserName("imurga");
-		user.setPassword("imurga");
-		user.setPossition("dev");
-		user.setEmail("murga@mur.com");
-		userRepo.save(user);
+        User user = new User();
+        user.setUserName("imurga");
+        user.setPassword("imurga");
+        user.setPossition("dev");
+        user.setEmail("murga@mur.com");
+        userRepo.save(user);
 
-		List<User> userList = (List<User>) userRepo.findAll();
-		for (User item : userList) {
-			System.out.println(item);
-		}
+        List<User> userList = (List<User>) userRepo.findAll();
+        for (User item : userList) {
+            System.out.println(item);
+        }
 
-		context.close();
+        context.close();
 
-	}
+    }
 }
